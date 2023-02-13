37bbf711bcfa2458223ce1acfe166b0141897dcc
Yaroslav Benin <benin@samawatt.com>
2023-02-13 17:12:03 +0400

    API - excetption handling and docstring.

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,7 +2,7 @@ import logging
 import uuid
 from logging import getLogger
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from kafka import KafkaProducer
 
@@ -28,20 +28,33 @@ def read_root():
 
 @app.post("/addview")
 def read_item(view: View):
-    # return {"item_id": item_id, "q": q}
+    """
+    An example for request JSON:
+    {
+    "user_uuid": "d16b19e7-e116-43b1-a95d-cd5a11e8f1b4",
+    "movie_uuid": "803c794c-ddf0-482d-b2c2-6fa92da4c5e2",
+    "topic": "views",
+    "value": 3921837
+    }
+
+    """
     logger.info('kafka address: %s', settings.kafka_host_port)
     print(f'kafka address: ', settings.kafka_host_port)
     producer = KafkaProducer(bootstrap_servers=[settings.kafka_host_port])
+    try:
+        for i in range(20):
+            # INPUT DATA
+            print("will insert: ", view)
+            producer.send(
+                topic=view.topic,
+                value=str(view.value).encode(),
+                key=f'{view.user_uuid}+{view.movie_uuid}'.encode(),
+            )
+    except Exception as e:
+        logger.error(e)
+        return HTTPException(status_code=500, detail=str(e))
+    finally:
+        producer.close()
 
-    for i in range(20):
-        # INPUT DATA
-        print("will insert: ", view)
-        producer.send(
-            topic=view.topic,
-            value=str(view.value).encode(),
-            key=f'{view.user_uuid}+{view.movie_uuid}'.encode(),
-        )
-
-    producer.close()
     return "OK"
 
