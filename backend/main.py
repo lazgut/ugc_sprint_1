4d6034799ec53faa4ab96bc0ea624b88b0b16527
Yaroslav Benin <benin@samawatt.com>
2023-02-13 15:14:40 +0400

    Some fixes.

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,17 @@
+import logging
 from typing import Union
 import random
 import uuid
+from logging import getLogger
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 from kafka import KafkaProducer
 
 from backend.config import settings
 
+logger = getLogger()
+logger.setLevel(logging.INFO)
 
 app = FastAPI()
 
@@ -26,15 +31,19 @@ def read_root():
 @app.post("/addview")
 def read_item(view: View):
     # return {"item_id": item_id, "q": q}
+    logger.info('kafka address: %s', settings.kafka_host_port)
+    print(f'kafka address: ', settings.kafka_host_port)
     producer = KafkaProducer(bootstrap_servers=[settings.kafka_host_port])
 
     for i in range(20):
         # INPUT DATA
+        print("will insert: ", view)
         producer.send(
             topic=view.topic,
-            value=view.value,
+            value=str(view.value).encode(),
             key=f'{view.user_uuid}+{view.movie_uuid}'.encode(),
         )
 
     producer.close()
+    return "OK"
 
