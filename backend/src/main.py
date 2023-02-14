cf9315d8e0b13f1b09ff748fc9bb0b0274d90d81
Yaroslav Benin <benin@samawatt.com>
2023-02-14 14:44:59 +0400

    Getting user_uuid from the request.

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,7 @@
 import logging
 from logging import getLogger
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 
 
 from models import View
@@ -30,26 +30,33 @@ async def shutdown_event():
 
 
 @app.post("/addview")
-def read_item(view: View):
+def add_view(view: View, request: Request):
     """
-    An example for request JSON:
+    An example request JSON:
     {
-    "user_uuid": "d16b19e7-e116-43b1-a95d-cd5a11e8f1b4",
+
     "movie_uuid": "803c794c-ddf0-482d-b2c2-6fa92da4c5e2",
     "topic": "views",
     "value": 3921837
     }
-
+    We assume that request headers contain used_uuid, after processing with authentication and middleware.
+    Headers:
+        ...
+        user_uuid: d16b19e7-e116-43b1-a95d-cd5a11e8f1b4
+        ...
     """
+    user_uuid = request.headers.get('user_uuid')
+    if not user_uuid:
+        raise HTTPException(401, detail='Unauthorized')
     try:
         producer.send(
             topic=view.topic,
             value=str(view.value).encode(),
-            key=f'{view.user_uuid}+{view.movie_uuid}'.encode(),
+            key=f'{user_uuid}+{view.movie_uuid}'.encode(),
         )
     except Exception as e:
         logger.error(e)
-        return HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail=str(e))
 
     return "OK"
 
