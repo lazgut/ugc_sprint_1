54ba4282d9e59d22f0239c1df6c25f084242178b
Yaroslav Benin <benin@samawatt.com>
2023-02-15 16:29:00 +0400

    Getting database records count.

--- investigation/client_send.py
+++ investigation/client_send.py
@@ -4,6 +4,7 @@ import uuid
 import requests
 
 import aiohttp
+from subprocess import Popen, PIPE
 
 
 def send_one_random():
@@ -34,7 +35,7 @@ async def main():
     async with aiohttp.ClientSession() as session:
 
         tasks = []
-        for number in range(1, 151):
+        for number in range(100000):
             main_url = 'http://127.0.0.1:8000'
             url = f'{main_url}/v1/addview'
             tasks.append(send_one_async(session, url))
@@ -44,5 +45,19 @@ async def main():
             print(one)
 
 
+def ask_records_count():
+    sql_cmd = 'SELECT COUNT(*) FROM cinema_ch'
+    docker_cmd = f'echo "{sql_cmd}" | clickhouse-client'
+    here_cmd = f"-ti clickhouse-node1 bash -c '{docker_cmd}'"
+    print(here_cmd)
+    with open('out', 'w') as outfile:
+        proc = Popen(args=['docker', 'exec', 'clickhouse-node1', 'bash', '-c', docker_cmd], stdout=PIPE)
+        # proc.wait()
+    #stdout = proc.communicate()[0]
+    result = proc.stdout.read()
+    return int(result)
+
+
 if __name__ == '__main__':
-    asyncio.run(main())
+    # asyncio.run(main())
+    print(ask_records_count())
