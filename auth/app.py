9c3dcb4e0076c27316dc5f7451448a674ba29ef1
RuslanYankov <115423892+RuslanYankov@users.noreply.github.com>
2023-03-03 17:18:49 +0300

    Added auth service (#39)
    * Added auth service
    
    * Auth complete

---
+++ auth/app.py
@@ -0,0 +1,22 @@
+import logging
+
+import logstash
+from flask_jwt_extended import JWTManager
+
+from config import settings
+from utils.app_factory import create_app
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(
+    logstash.LogstashHandler(settings.logstash_host, settings.logstash_port, version=1, tags='auth')
+)
+
+
+app = create_app(settings)
+jwt = JWTManager(app)
+
+if __name__ == "__main__":
+    from config import settings
+    app.run(host=settings.host, port=settings.port)
---
+++ auth/utils/namespaces/login_google.py
@@ -0,0 +1,4 @@
+from flask_restx import Namespace
+
+
+ns = Namespace("Login with Google", description="Here the user can authorization with Google")
